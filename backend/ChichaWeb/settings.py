59e43bbd2e5223fe63c882ecb44ddc749637cd1a
SlimShady9 <slim.shady99q@gmail.com>
2021-10-24 19:06:17 -0500

    Prueba database

--- backend/ChichaWeb/settings.py
+++ backend/ChichaWeb/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -111,6 +113,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
